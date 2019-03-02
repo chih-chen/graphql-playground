1ea2d353e1d3f576dd8e4701b7f6060cfefde5a0
chih-chen <chih.chen1029@gmail.com>
2019-03-02 20:18:42 -0300

    create account repo

---
+++ src/main/kotlin/repository/account/AccountRepository.kt
@@ -0,0 +1,16 @@
+package repository.account
+
+import graphql.types.Account
+
+class AccountRepository {
+
+    private val accounts = listOf(
+        Account("account1", "bank1", "password1"),
+        Account("account2", "bank2", "password2"),
+        Account("account3", "bank3", "password3"),
+        Account("account4", "bank4", "password4"),
+        Account("account5", "bank5", "password5"),
+        Account("account6", "bank6", "password6"),
+        Account("account7", "bank7", "password7")
+    )
+}
